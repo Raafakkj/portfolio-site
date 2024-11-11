793b4ffb833becba5b646458a21812d6dd306202
Raafakkj <rafinharamosgomes10@gmail.com>
2024-11-11 15:42:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+window.addEventListener('load', () => {
+    const content = document.getElementById("content");
+    content.classList.add("fade-in");
+});
+
+
+window.addEventListener('load', () => {
+    const content = document.getElementById("content2");
+    content.classList.add("fade-in");
+});
