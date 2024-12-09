a81f2c9aa4e80cafb39190d58e2658cab2755bda
comet400 <Lukas.fvieira@hotmail.com>
2024-12-08 21:29:02 -0500

    function reduction

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 
 window.addEventListener('load', () => {
     const content = document.getElementById("content");
+    const content2 = document.getElementById("content2");
     content.classList.add("fade-in");
+    content2.classList.add("fade-in");
 });
 
-
-window.addEventListener('load', () => {
-    const content = document.getElementById("content2");
-    content.classList.add("fade-in");
-});
